6fccea0e7ac8c8468d85367e92385b56f717a871
Nguyn <phamnguyen0234@gmail.com>
2025-10-18 10:46:04 +0700

    change the config

--- login/config.php
+++ login/config.php
@@ -1,5 +1,15 @@
 <?php
-// session_start();
-$conn = mysqli_connect("localhost","root","","music");
+// Thông tin kết nối
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'music';
 
+// Kết nối đến MySQL
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Kiểm tra kết nối
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 ?>
